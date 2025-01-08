172ba7a99d6c471086a3ca438f42268372efb88e
limosusbiscuit <jaedonallen11@gmail.com>
2025-01-07 20:34:01 -0600

    Basic resume for testing angular

---
+++ src/app/services-home/services-home.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+
+@Component({ 
+ selector: 'app-services-home', 
+ standalone: true, 
+ imports: [], 
+ templateUrl: './services-home.component.html', 
+ styleUrl: './services-home.component.css'
+})
+
+export class ServicesHomeComponent {
+
+}
